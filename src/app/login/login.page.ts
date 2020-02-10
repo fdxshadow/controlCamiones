953dcf6de0c52f5c617fdcc7a6ebc3aa3b630716
Avalora Fpena <avalorafpena@MacBook-Pro-de-Avalora.local>
2020-02-10 09:09:57 -0300

    probando workflows

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+
+  user = {};
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  login(){
+    console.log(this.user);
+  }
+
+}

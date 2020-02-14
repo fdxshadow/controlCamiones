f51a1e26e7d4ad0bb4e5e95d6dea11769c7dfc25
fdxshadow <fran.peagonza@gmail.com>
2020-02-14 17:06:56 +0000

    validacion minima login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule,ReactiveFormsModule,FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import {FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
